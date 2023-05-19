354a439b8004f3eb2165e1c7c039f2e050d8e022
nono742 <noam9290@gmail.com>
2023-05-19 17:22:50 +0200

    cavalier_v3

--- src/AffichableOnMap/cavalier.cpp
+++ src/AffichableOnMap/cavalier.cpp
@@ -1,1 +1,45 @@
 #include "cavalier.hpp"
+#include "../Commande/choix_se_deplacer.hpp"
+#include "../Commande/choix_attaquer.hpp"
+
+const std::string Cavalier::TEXTURE_FILE_NAME = "cavalier_icone.png";
+const std::string Cavalier::INFO = "vitesse de déplacement très élevée";
+const std::string Cavalier::NAME ="Cavalier";
+
+const std::string Cavalier::ICONE_TEXTURE_FILE_NAME = "Cavalier_icone.png";
+
+
+
+
+int Cavalier::nombreActionBonus = 0;
+int Cavalier::nombrePvBonus = 0;
+float Cavalier::mombreCaseDeplacementBonus=0;
+int Cavalier::nombreDegatBonus=0;
+float Cavalier::distanceAttaqueBonus=0;
+
+
+
+
+Cavalier::Cavalier(Joueur_ptr joueurProprietaire) : Unite(TEXTURE_FILE_NAME,INFO,NAME,ICONE_TEXTURE_FILE_NAME,joueurProprietaire) {
+
+
+}
+
+ListPtrCommandeSquare_ptr Cavalier::getActionPossible(){
+    ListPtrCommandeSquare_ptr commandes = std::make_shared<std::list<CommandeSquare *>>();
+    commandes->push_back(new ChoixSeDeplacer(this)); // car herite de InterfaceSePlacer
+    commandes->push_back(new ChoixAttaquer(this)); // car herite de InterfaceAttaquer
+    return commandes;
+
+}
+
+
+void Cavalier::ressourcePourAchat(int &bois, int &nourriture, int &_or) {
+    bois = COUT_ACHAT_BOIS;
+    nourriture = COUT_ACHAT_NOURRITURE;
+    _or = COUT_ACHAT_OR;
+}
+
+void Cavalier::nouveauTour() {
+    resetNombreAction();
+}
